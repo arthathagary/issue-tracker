159a85b4b8aa95e9c9e63b950c993a775035b539
Arthath Agary <arthathagary@gmail.com>
2023-10-28 20:47:58 +0530

    create a delete api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -30,3 +30,18 @@ export async function PATCH(reqest:NextRequest,
         return NextResponse.json(updatedIssue,{status:200});
 
 }
+
+export async function DELETE(reqest:NextRequest,{params}:{params:{id:string}}){
+    const issue = await prisma.issue.findUnique({
+        where: {id:parseInt(params.id)}
+    })
+
+    if(!issue)
+        return NextResponse.json({error:'Not a valid issue'},{status:404})
+
+    await prisma.issue.delete({
+        where:{id:issue.id}
+    })
+
+    return NextResponse.json({},{status:200})
+}
