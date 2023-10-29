65e4c867d1f9ece8a53063dd8e7a781dceb22171
Arthath Agary <arthathagary@gmail.com>
2023-10-29 13:02:53 +0530

    build select assignee

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -10,7 +10,7 @@ const authOptions : NextAuthOptions = {
           clientId: process.env.GOOGLE_CLIENT_ID!,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-      ]
+      ],
 }
 
 export default authOptions;
---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,23 @@
+"use client"
+import {Select} from '@radix-ui/themes'
+
+
+const AssigneeSelect = () => {
+  return (
+    <Select.Root>
+        <Select.Trigger placeholder='Assign...' />
+            <Select.Content>
+            <Select.Group>
+                <Select.Label>
+                    Suggestions
+                </Select.Label>
+                <Select.Item value='1'>
+                    Agary
+                </Select.Item>
+            </Select.Group>
+            </Select.Content>
+    </Select.Root>
+  )
+}
+
+export default AssigneeSelect
