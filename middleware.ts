a3c8fd031dd4d8e6613e7a7ce85fc42aa4f57091
Arthath Agary <arthathagary@gmail.com>
2023-11-21 12:45:20 +0530

    Middleware changed

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ export {default} from 'next-auth/middleware';
 
 export const config = {
     matcher : [
-        '/issues/list/new',
+        '/issues/new',
         '/issues/list/edit/:id+'
     ]
 }
